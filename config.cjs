58bca71b41d3c335b93a546e08713d6aa6810aea
Njabulo Jb <fanajb65@gmail.com>
2026-02-27 11:35:13 +0200

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -19,7 +19,7 @@ const config = {
   REJECT_CALL: process.env.REJECT_CALL !== undefined ? process.env.REJECT_CALL === 'true' : false, 
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
   MODE: process.env.MODE || "public",
-  BOT_NAME: process.env.BOT_NAME || "njabulojb-elite",
+  BOT_NAME: process.env.BOT_NAME || "GWM-XMD",
   MENU_IMAGE: process.env.MENU_IMAGE || "https://files.catbox.moe/vy8uyv.jpg",
   DESCRIPTION: process.env.DESCRIPTION || "njabulo",
   OWNER_NAME: process.env.OWNER_NAME || "XD JB",
