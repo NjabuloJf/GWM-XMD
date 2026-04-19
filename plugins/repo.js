11e1b10203ee9569ce2e71fe544b57a3f6802f3e
Njabulo Jb <fanajb65@gmail.com>
2026-04-19 23:33:57 +0200

    Enhance repo function with video message support
    Refactor repo function to send video messages along with repo info.

--- plugins/repo.js
+++ plugins/repo.js
@@ -1,6 +1,8 @@
 
+
 import axios from "axios";
 import config from '../config.cjs';
+import fs from "fs";
 
 const repo = async (m, gss) => {
   const prefix = config.PREFIX;
@@ -11,14 +13,48 @@ const repo = async (m, gss) => {
       const response = await axios.get(`https://api.github.com/repos/NjabuloJ/Njabulo-Jb`);
       const data = response.data;
       const msg = `
-*Repo Name:* ${data.name}
-*Stars:* ${data.stargazers_count}
-*Forks:* ${data.forks_count}
-*Description:* ${data.description}
-*Repo Link:* ${data.html_url}
+*┌─❖*
+*│𝐆𝐖𝐌-𝐗𝐌𝐃*
+*└┬❖*
+   *│🔕${m.pushName}*
+   *└────────┈❖*
+▬▬▬▬▬▬▬▬▬▬
+> 🕵️ ${data.stargazers_count} stars
+> 📅 ${data.forks_count} forks
+> ⏰ ${data.description}
+> ⭐ ${data.html_url}
+▬▬▬▬▬▬▬▬▬▬
       `;
-      await gss.sendMessage(m.from, { text: msg });
       await gss.sendMessage(m.from, { react: { text: "👍", key: m.key } });
+      await gss.sendMessage(m.from, { video: fs.readFileSync('./public/menuvidei.mp4'), mimetype: 'video/mp4', ptv: true });
+      await gss.sendMessage(m.from, { 
+        video: fs.readFileSync('./public/menuvid.mp4'), 
+        caption: msg, 
+        contextInfo: { 
+          mentionedJid: [m.sender], 
+          forwardingScore: 999, 
+          isForwarded: true, 
+          forwardedNewsletterMessageInfo: { 
+            newsletterJid: config.ID_CHANNEL, 
+            newsletterName: "╭••➤GWM-XMD", 
+            serverMessageId: 143, 
+          } 
+        } 
+      }, { 
+        quoted: { 
+          key: { 
+            fromMe: false, 
+            participant: `0@s.whatsapp.net`, 
+            remoteJid: "status@broadcast" 
+          }, 
+          message: { 
+            contactMessage: { 
+              displayName: `${m.pushName}`, 
+              vcard: `BEGIN:VCARD\nVERSION:3.0\nN:Njabulo-Jb;BOT;;;\nFN:Njabulo-Jb\nitem1.TEL;waid=26777821911:+26777821911\nitem1.X-ABLabel:Bot\nEND:VCARD` 
+            } 
+          } 
+        } 
+      });
     } catch (error) {
       console.error(error);
       await gss.sendMessage(m.from, { text: "Error fetching repo info 😕" });
