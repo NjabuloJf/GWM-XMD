1270fff00dd3e2afae4866bb41df12e3d99c9a96
Njabulo Jb <fanajb65@gmail.com>
2026-05-03 00:15:33 +0200

    Rename downloads function to download-menu

--- plugins/downloadmenu.js
+++ plugins/downloadmenu.js
@@ -32,12 +32,12 @@ const fetchGitHubData = async (owner, repo) => {
   }
 };
 
-const downloads = async (m, Matrix) => {
+const download-menu = async (m, Matrix) => {
   try {
     const prefix = config.PREFIX || '!'; // Default prefix if not configured
     const cmd = m.body?.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
 
-    if (cmd === "gdownload" || cmd === "download-menu" || cmd === "github") {
+    if (cmd === "gdownload" || cmd === "download-menu" || cmd === "download-menu" ) {
       console.log('Repository command triggered!');
       
       const repoImages = "https://raw.githubusercontent.com/NjabuloJf/Njabulo-Jb/main/public/fanaa.jpg";
@@ -291,4 +291,4 @@ try {
   }
 };
 
-export default downloads;
+export default downloadmenu;
