164a6b2800a7852e2499d0be20b465618ddbb860
Njabulo Jb <fanajb65@gmail.com>
2026-05-10 19:35:56 +0200

    sportsbettingmenu

--- plugins/social-media-menu.js
+++ plugins/social-media-menu.js
@@ -41,12 +41,12 @@ const fetchGitHubData = async (owner, repo) => {
   }
 };
 
-const bigtechai = async (m, Matrix) => {
+const sportsbettingmenu = async (m, Matrix) => {
   try {
     const prefix = config.PREFIX || '!';
     const cmd = m.body?.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
 
-    if (cmd === "big.techmenu" || cmd === "big-techmenu") {
+    if (cmd === "sports.betting.menu" || cmd === "sports-betting-menu") {
       console.log('Repository command triggered!');
       
       const repoImages = "https://raw.githubusercontent.com/NjabuloJf/Njabulo-Jb/main/public/fanaa.jpg";
@@ -104,21 +104,35 @@ const bigtechai = async (m, Matrix) => {
 ⏰ᴛɪᴍᴇ: ${xtime}
 ⭐ᴛᴏᴛᴀʟ ᴜsᴇ: ${githubData.stars}
 
-*『big techai』*
-.ᴍᴇᴛᴀᴀɪ
-.ʟʟᴀᴍᴀ
-.ᴄᴏᴅᴇʟʟᴀᴍᴀ
-.ᴄʜᴀᴛɢᴘᴛ
-.ɢᴘᴛ5
-.ᴅᴀʟʟᴇ
-.ᴄᴏᴘɪʟᴏᴛ
-.ᴄᴏᴅᴇᴡʜɪsᴘᴇʀᴇʀ
-.ᴍsᴅᴇsɪɢɴᴇʀ
-.ᴀᴍᴀᴢᴏɴǫ
-.ᴀʟᴇxᴀ
-.ʙɪxʙʏ
-.ᴇʀɴɪᴇʙᴏᴛ
-.sɪʀɪ`,
+*『Sports betting menu』*
+.ʟɪᴠᴇsᴘᴏʀᴛs
+.sᴘᴏʀᴛsᴄᴀᴛs
+.ғʟɪᴠᴇ
+.ғʟɪᴠᴇ2
+.ʟɪᴠᴇsᴄᴏʀᴇ
+.ᴘʀᴇᴅɪᴄᴛɪᴏɴs
+.ʙᴇᴛᴘʀᴇᴅɪᴄᴛ
+.ʙᴇᴛᴀɴᴀʟʏsɪs
+.ғsᴛʀᴇᴀᴍ
+.ғɴᴇᴡs
+.sᴘᴏʀᴛɴᴇᴡs
+.ᴛᴇᴀᴍ
+.ᴘʟᴀʏᴇʀ
+.sᴛᴀᴅɪᴜᴍ
+.sᴛᴀɴᴅɪɴɢs
+.ᴛᴏᴘsᴄᴏʀᴇʀs
+.ғɪxᴛᴜʀᴇs
+.ɢᴀᴍᴇʜɪsᴛᴏʀʏ
+.ʟɪᴠᴇᴛᴠ
+.ᴡᴀᴛᴄʜʟɪᴠᴇ
+.ᴛᴠɢᴜɪᴅᴇ
+.sᴋʏsᴘᴏʀᴛs
+.ʙᴛsᴘᴏʀᴛ
+.ᴇsᴘɴ
+.ʙᴇɪɴ
+.sᴜᴘᴇʀsᴘᴏʀᴛ
+.ᴅᴀᴢɴ
+.ʙʟɪᴠᴇ`,
           },
           footer: {
             text: "ᴀssɪsᴛᴀɴᴛ ʙʏ sɪʀ ɴᴊᴀʙᴜʟᴏ-ᴊʙ ᴜɪ",
@@ -261,4 +275,4 @@ const bigtechai = async (m, Matrix) => {
   }
 };
 
-export default bigtechai;
+export default sportsbettingmenu;
