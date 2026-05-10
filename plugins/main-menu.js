73c514c790b39679497f1a7eed9bf8b812096e59
Njabulo Jb <fanajb65@gmail.com>
2026-05-10 23:54:40 +0200

    Rename bigtechai to mainmenu in main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -41,12 +41,12 @@ const fetchGitHubData = async (owner, repo) => {
   }
 };
 
-const bigtechai = async (m, Matrix) => {
+const mainmenu = async (m, Matrix) => {
   try {
     const prefix = config.PREFIX || '!';
     const cmd = m.body?.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
 
-    if (cmd === "big.techmenu" || cmd === "big-techmenu") {
+    if (cmd === "main-menu" || cmd === "main.menu") {
       console.log('Repository command triggered!');
       
       const repoImages = "https://raw.githubusercontent.com/NjabuloJf/Njabulo-Jb/main/public/fanaa.jpg";
@@ -104,21 +104,27 @@ const bigtechai = async (m, Matrix) => {
 ⏰ᴛɪᴍᴇ: ${xtime}
 ⭐ᴛᴏᴛᴀʟ ᴜsᴇ: ${githubData.stars}
 
-*『big techai』*
-.ᴍᴇᴛᴀᴀɪ
-.ʟʟᴀᴍᴀ
-.ᴄᴏᴅᴇʟʟᴀᴍᴀ
-.ᴄʜᴀᴛɢᴘᴛ
-.ɢᴘᴛ5
-.ᴅᴀʟʟᴇ
-.ᴄᴏᴘɪʟᴏᴛ
-.ᴄᴏᴅᴇᴡʜɪsᴘᴇʀᴇʀ
-.ᴍsᴅᴇsɪɢɴᴇʀ
-.ᴀᴍᴀᴢᴏɴǫ
-.ᴀʟᴇxᴀ
-.ʙɪxʙʏ
-.ᴇʀɴɪᴇʙᴏᴛ
-.sɪʀɪ`,
+*『main menu』*
+.ᴄʀᴇᴡ
+.ᴅᴇʟᴇᴛᴇ
+.ᴇʙɪɴᴀʀʏ
+.ᴇᴍᴏᴊɪᴍɪx
+.ғᴇᴛᴄʜᴅᴀᴛᴀ
+.ɢᴄᴇᴠᴇɴᴛ
+.ɢɪᴛʜᴜʙsᴛᴀʟᴋ
+.ɢɪᴠᴇᴛᴇxᴛ
+.ʜᴀɴᴅʟᴇ
+.ɪᴍᴀɢᴇ
+.ʟᴏɢᴏ
+.ᴍɪsᴛʀᴀʟ
+.ǫᴜᴏᴛᴇᴅᴄʜᴀᴛ
+.ʀᴇᴀᴅǫʀ
+.sᴛɪᴄᴋᴇʀ
+.ᴛᴏʜᴅ
+.ᴛᴏᴍᴘ3
+.ᴛᴏǫʀ
+.ᴛᴏᴜʀʟ
+.ᴛʀᴀɴsʟᴀᴛᴇ`,
           },
           footer: {
             text: "ᴀssɪsᴛᴀɴᴛ ʙʏ sɪʀ ɴᴊᴀʙᴜʟᴏ-ᴊʙ ᴜɪ",
@@ -261,4 +267,4 @@ const bigtechai = async (m, Matrix) => {
   }
 };
 
-export default bigtechai;
+export default mainmenu;
