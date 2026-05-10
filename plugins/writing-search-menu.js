6a3788fea0f7d011de399428cf2f93a4662038f1
Njabulo Jb <fanajb65@gmail.com>
2026-05-10 19:57:14 +0200

    Rename bigtechai to writingsearchmenu

--- plugins/writing-search-menu.js
+++ plugins/writing-search-menu.js
@@ -41,12 +41,12 @@ const fetchGitHubData = async (owner, repo) => {
   }
 };
 
-const bigtechai = async (m, Matrix) => {
+const writingsearchmenu = async (m, Matrix) => {
   try {
     const prefix = config.PREFIX || '!';
     const cmd = m.body?.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
 
-    if (cmd === "big.techmenu" || cmd === "big-techmenu") {
+    if (cmd === "writing-search-menu" || cmd === "writing.search.menu") {
       console.log('Repository command triggered!');
       
       const repoImages = "https://raw.githubusercontent.com/NjabuloJf/Njabulo-Jb/main/public/fanaa.jpg";
@@ -104,21 +104,28 @@ const bigtechai = async (m, Matrix) => {
 ⏰ᴛɪᴍᴇ: ${xtime}
 ⭐ᴛᴏᴛᴀʟ ᴜsᴇ: ${githubData.stars}
 
-*『big techai』*
-.ᴍᴇᴛᴀᴀɪ
-.ʟʟᴀᴍᴀ
-.ᴄᴏᴅᴇʟʟᴀᴍᴀ
-.ᴄʜᴀᴛɢᴘᴛ
-.ɢᴘᴛ5
-.ᴅᴀʟʟᴇ
-.ᴄᴏᴘɪʟᴏᴛ
-.ᴄᴏᴅᴇᴡʜɪsᴘᴇʀᴇʀ
-.ᴍsᴅᴇsɪɢɴᴇʀ
-.ᴀᴍᴀᴢᴏɴǫ
-.ᴀʟᴇxᴀ
-.ʙɪxʙʏ
-.ᴇʀɴɪᴇʙᴏᴛ
-.sɪʀɪ`,
+*『writing search menu』*
+.ɢʀᴏᴋ
+.ᴘᴇʀᴘʟᴇxɪᴛʏ
+.ᴘɪ
+.ʏᴏᴜᴄᴏᴍ
+.ʏɪᴄʜᴀᴛ
+.ᴄʜᴀʀᴀᴄᴛᴇʀᴀɪ
+.ᴘᴏᴇ
+.ʜᴜɢɢɪɴɢᴄʜᴀᴛ
+.ᴊᴀsᴘᴇʀ
+.ᴄᴏᴘʏᴀɪ
+.ᴡʀɪᴛᴇsᴏɴɪᴄ
+.ʀʏᴛʀ
+.ǫᴜɪʟʟʙᴏᴛ
+.sᴜᴅᴏᴡʀɪᴛᴇ
+.ɴᴏᴠᴇʟᴀɪ
+.ɢʀᴀᴍᴍᴀʀʟʏɢᴏ
+.ʜʏᴘᴇʀᴡʀɪᴛᴇ
+.ᴡᴏʀᴅᴛᴜɴᴇ
+.ᴍᴇᴍᴀɪ
+.ᴄᴏɴsᴇɴsᴜs
+.ᴇʟɪᴄɪᴛ`,
           },
           footer: {
             text: "ᴀssɪsᴛᴀɴᴛ ʙʏ sɪʀ ɴᴊᴀʙᴜʟᴏ-ᴊʙ ᴜɪ",
@@ -261,4 +268,4 @@ const bigtechai = async (m, Matrix) => {
   }
 };
 
-export default bigtechai;
+export default writingsearchmenu;
